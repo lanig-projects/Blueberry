ae8c4e3cddadb35b72d4ef2c668673bd0d6a9e4e
Mauricio Martinez <mtz0mau2002@gmail.com>
2025-02-23 15:10:09 -0600

    feat: enhance WelcomePage with styled content and layout

--- src/app/@default/pages/WelcomePage/Welcome.page.tsx
+++ src/app/@default/pages/WelcomePage/Welcome.page.tsx
@@ -4,8 +4,13 @@ export const WelcomePage = () => {
   useDocumentTitle('Welcome');
 
   return (
-    <div>
-      WelcomePage
+    <div className={'h-screen flex justify-center items-center bg-neutral-900'}>
+      <div className={'text-white'}>
+        <h1 className={'text-4xl font-bold'}>
+          Welcome to <span className={'font-extrabold text-primary'}>Blueberry</span>
+        </h1>
+        <p className={'text-lg'}>This is a starter template for React applications.</p>
+      </div>
     </div>
   );
 };
